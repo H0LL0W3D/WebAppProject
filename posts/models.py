e2c3f89ca6987ea623706848444c3425a98b3b05
Andy Wolf <ndrwwlf@gmail.com>
2022-11-15 16:37:17 -0700

    through chap 15

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from crispy_forms.helper import FormHelper
 
 
 class Post(models.Model):
@@ -17,3 +18,18 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse("post_list")
--- posts/views.py
+++ posts/views.py
@@ -1,22 +1,71 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views import View
+from django.views.generic import ListView, DetailView, FormView
+from django.views.generic.detail import SingleObjectMixin
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import render
 
 from .models import Post
+from .forms import CommentForm
 
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = "post_list.html"
 
 
-class PostDetailView(DetailView):
+# class PostDetailView(LoginRequiredMixin, DetailView):
+#     model = Post
+#     template_name = "post_detail.html"
+
+#     def get_context_data(self, **kwargs):  # new
+#         context = super().get_context_data(**kwargs)
+#         context["form"] = CommentForm()
+#         return context
+
+
+class CommentGet(DetailView):
     model = Post
     template_name = "post_detail.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
 
-class PostUpdateView(UpdateView):
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Post
+    form_class = CommentForm
+    template_name = "post_detail.html"
+
+    def post(self, reqeust, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(reqeust, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse("post_detail", kwargs={"pk": post.pk})
+
+
+class PostDetailView(LoginRequiredMixin, View):  # new
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = (
         "title",
@@ -24,21 +73,32 @@ class PostUpdateView(UpdateView):
     )
     template_name = "post_edit.html"
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class PostDeleteView(DeleteView):
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "post_delete.html"
     success_url = reverse_lazy("post_lsit")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "post_new.html"
     fields = (
         "title",
         "body",
-        "author",
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 # Create your views here.
